d69d8590bad1f9324648acdd1ea9b8023f7396c8
jtran632 <jimmytran357@yahoo.com>
2023-07-11 19:43:22 -0700

    added pokemon, trainer, and energy card filtering when viewing set

--- src/app/components/displaySetCards.tsx
+++ src/app/components/displaySetCards.tsx
@@ -5,35 +5,63 @@ import { useRouter } from "next/navigation";
 export default function DisplaySetCards({ cards }: any) {
   // console.log("inside display set cards", cards);
   const router = useRouter();
+  const [toggleType, setToggleType] = useState("");
   function CreateCard(i: any) {
-    const [modal, setModal] = useState(i);
-    const [toggle, setToggle] = useState(false);
-    const toggleChecked = () => setToggle((value) => !value);
     console.log("current card", i.name, i);
-    return (
-      <button
-        key={i.id}
-        className={`flex flex-col justify-center items-center hover:rounded-md hover:bg-gradient-to-r from-red-300 via-green-300 to-blue-300 p-1 ${
-          toggle ? "animate-spin" : ""
-        } `}
-        onClick={() => router.push(`/card/${i.id}`)}
-      >
-        <Image
-          src={`${i.images.large}`}
-          alt={"pokemon image"}
-          width={300}
-          height={100}
-          loading="lazy"
-        ></Image>
-      </button>
+    const CardElement = () => {
+      return (
+        <button
+          key={i.id}
+          className={`flex flex-col justify-center items-center hover:rounded-md hover:bg-gradient-to-r from-red-300 via-green-300 to-blue-300 p-1`}
+          onClick={() => router.push(`/card/${i.id}`)}
+          type={i.supertype}
+        >
+          <Image
+            src={`${i.images.small}`}
+            alt={"pokemon image"}
+            width={300}
+            height={100}
+            loading="lazy"
+          ></Image>
+        </button>
+      );
+    };
+    return toggleType === "" ? (
+      <CardElement />
+    ) : toggleType === i.supertype ? (
+      <CardElement />
+    ) : (
+      <></>
     );
   }
 
+  const supertypes: string[] = ["Pokémon", "Trainer", "Energy"];
+  console.log(toggleType);
   return (
-    <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
-      {cards.map((i: any) => {
-        return CreateCard(i);
-      })}
+    <div>
+      <div className="flex justify-center gap-10 text-black">
+        {supertypes.map((i) => {
+          return (
+            <button
+              key={i}
+              value={i}
+              className={`border border-black px-3 mb-6 rounded-md font-bold ${
+                toggleType === i ? "bg-emerald-300 " : " bg-white"
+              }`}
+              onClick={() =>
+                toggleType === i ? setToggleType("") : setToggleType(i)
+              }
+            >
+              {i}
+            </button>
+          );
+        })}
+      </div>
+      <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+        {cards.map((i: any) => {
+          return CreateCard(i);
+        })}
+      </div>
     </div>
   );
 }
--- src/app/sets/[id]/page.tsx
+++ src/app/sets/[id]/page.tsx
@@ -1,18 +1,15 @@
 import DisplaySetCards from "@/app/components/displaySetCards";
 async function getSetCards(id: string) {
   let url = `https://api.pokemontcg.io/v2/cards?q=set.id:${id}`;
-  console.log(id)
-  console.log(url);
   const a = await fetch(url);
   return a.json();
 }
 export default async function SetPageWithName({ params }: any) {
   let cards = await getSetCards(params.id);
-  console.log(params)
-  console.log(cards);
+  console.log("cards", cards);
   //   return <DisplaySetCards cards={cards} />;
   return (
-    <div className="bg-white min-h-screen h-full p-20">
+    <div className="bg-white min-h-screen h-full px-20 pt-10 pb-20">
       <DisplaySetCards cards={cards.data}></DisplaySetCards>
     </div>
   );
