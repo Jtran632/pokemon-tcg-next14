bf85bc0d17e31df68886501fff4c2f2cb3fb196f
jtran632 <jimmytran357@yahoo.com>
2024-04-15 10:28:51 -0700

    navbar adjustments

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
-'use client'
+"use client";
 import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="flex items-center justify-between">
       <img
-        className="-rotate-12"
+        className="-rotate-12 sm:w-[50px]"
         src={"/icon.png"}
         alt="header image"
         width={100}
         height={"auto"}
       ></img>
-      <div className="flex w-full place-content-between justify-center text-6xl lg:text-4xl md:text-3xl sm:text-2xl xs:text-base gap-10 p-4">
+      <div className="flex w-full place-content-between justify-center text-6xl lg:text-4xl md:text-3xl sm:text-2xl xs:text-base gap-10 sm:gap-4 p-4">
         <Link href={"/"}>Home </Link>
         <Link href={"/sets"}>Sets</Link>
         <Link href={"/collection"}>Collection</Link>
