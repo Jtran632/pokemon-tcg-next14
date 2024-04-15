a08c4ebd399911efc08bd234683f78337b4848df
jtran632 <jimmytran357@yahoo.com>
2024-04-15 00:03:30 -0700

    added trpc route.ts context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,13 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { postRouter } from "@/server/api/routers/posts";
 import { type NextRequest } from "next/server";
+import { createContext } from "@/server/trpc/server";
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: postRouter,
-    //@ts-ignore
-    createContext: () => ({}),
+    createContext,
   });
 
 export { handler as GET, handler as POST };
