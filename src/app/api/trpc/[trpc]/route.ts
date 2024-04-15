fa66a9c85c33452cfcb5beda02e10833ec0c06db
jtran632 <jimmytran357@yahoo.com>
2024-04-15 00:06:13 -0700

    minor change

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,7 +1,17 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { postRouter } from "@/server/api/routers/posts";
 import { type NextRequest } from "next/server";
-import { createContext } from "@/server/trpc/server";
+import { headers } from "next/headers";
+import { cache } from "react";
+import { createTRPCContext } from "@/server/api/trpc";
+const createContext = cache(() => {
+  const heads = new Headers(headers());
+  heads.set("x-trpc-source", "rsc");
+
+  return createTRPCContext({
+    headers: heads,
+  });
+});
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
