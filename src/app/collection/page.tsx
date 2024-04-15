627f6bf08e605e8f7bf7208414127600b9fd81e4
jtran632 <jimmytran357@yahoo.com>
2024-04-15 02:04:10 -0700

    updated next to latest

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -4,9 +4,9 @@ import { absoluteUrl } from "@/lib/utils";
 export default async function Collection() {
   let favs = [];
   favs = await getFavs();
+  console.log(absoluteUrl())
   return (
     <div className="flex min-h-screen w-full px-20 sm:px-10 xs:px-10 py-10 text-black bg-white">
-      <div className="text-white">test : {absoluteUrl()}</div>
       <DisplayFavs favs={favs} />
     </div>
   );
