718c4a4f4b3ed82f781e922579004e95a8e3704b
jtran632 <jimmytran357@yahoo.com>
2024-04-15 00:18:54 -0700

    changed trpc route.ts context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,24 +1,30 @@
-'use client'
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+"use client";
+import {
+  FetchCreateContextFnOptions,
+  fetchRequestHandler,
+} from "@trpc/server/adapters/fetch";
 import { postRouter } from "@/server/api/routers/posts";
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
-import { cache } from "react";
-import { createTRPCContext } from "@/server/api/trpc";
-const createContext = cache(() => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
-
-  return createTRPCContext({
-    headers: heads,
-  });
-});
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { Session } from "next-auth";
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: postRouter,
-    createContext,
+    createContext: function (opts: FetchCreateContextFnOptions):
+      | {
+          headers: Headers;
+          db: PostgresJsDatabase<typeof import("../../../../db/schema")>;
+          session: Session | null;
+        }
+      | Promise<{
+          headers: Headers;
+          db: PostgresJsDatabase<typeof import("../../../../db/schema")>;
+          session: Session | null;
+        }> {
+      throw new Error("Function not implemented.");
+    },
   });
 
 export { handler as GET, handler as POST };
