c3439fde388af8439e3772a77d8888624e2180dd
jtran632 <jimmytran357@yahoo.com>
2024-04-15 02:20:56 -0700

    .

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,10 +1,10 @@
 import DisplayFavs from "@/app/components/displayFavs";
 import { getFavs } from "@/lib/actions";
-import { absoluteUrl } from "@/lib/utils";
 export default async function Collection() {
-  let favs = [];
-  favs = await getFavs();
-  console.log(absoluteUrl())
+  let favs = await getFavs();
+  if (!favs) {
+    <div>Loading...</div>;
+  }
   return (
     <div className="flex min-h-screen w-full px-20 sm:px-10 xs:px-10 py-10 text-black bg-white">
       <DisplayFavs favs={favs} />
