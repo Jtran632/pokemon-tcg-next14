0ff8698b1fa076ed1a2adc413b5d013a270eaaf0
jtran632 <jimmytran357@yahoo.com>
2024-07-24 22:07:00 -0700

    minor change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,7 @@ export default async function LoginPage() {
           >
             {session ? "Sign Out" : "Sign In"}
           </Link>
-          {!session ? (
+          {/* {!session ? (
             <Link
               href={"/api/auth/signup"}
               className="rounded-md bg-white/10 px-10 py-2 font-semibold no-underline transition hover:bg-white/20"
@@ -22,7 +22,7 @@ export default async function LoginPage() {
             </Link>
           ) : (
             <></>
-          )}
+          )} */}
         </div>
       </div>
     </main>
