a13528feab7f145bc5d2452fed2e735bc35692d6
jtran632 <jimmytran357@yahoo.com>
2025-05-05 00:59:08 -0700

    .

--- src/app/card/[id]/page.tsx
+++ src/app/card/[id]/page.tsx
@@ -1,16 +1,12 @@
 import DisplayCardInfo from "@/app/components/displayCardInfo";
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 async function getCard(id: string) {
   let url = `https://api.pokemontcg.io/v2/cards/${id}`;
   const a = await fetch(url);
   return a.json();
 }
 
-export default async function SetPageWithCardName({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function SetPageWithCardName({ params }: { params: any }) {
   let i = await getCard(params.id);
   return (
     <div>
