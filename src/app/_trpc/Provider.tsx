869a22f805152042ced23e74fdd418d4fb21c7cb
jtran632 <jimmytran357@yahoo.com>
2025-05-11 11:37:10 -0700

    .

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { loggerLink, httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
